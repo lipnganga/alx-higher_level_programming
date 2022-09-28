7bcbb388c27d5ccd1028dee87e18e294623f01bc
lipnganga <philipmurage98@gmail.com>
2022-09-28 23:49:20 +0300

    task 1 commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if num != search:
-        else replace:
-    for num in my_list:
-        return num:
+    return ([num if num != search  else replace for num in my_list])
