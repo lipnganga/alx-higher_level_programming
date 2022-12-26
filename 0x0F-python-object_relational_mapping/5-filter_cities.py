0d4d4e5e1ba8aeca573383bb96bf56439e14938d
lipnganga <107026504+lipnganga@users.noreply.github.com>
2022-12-26 22:51:05 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,43 +1,37 @@
-
-
 #!/usr/bin/python3
 """Displays all cities of arguments state"""
-import MySQLdb
-import sys
-
-
-def list_cities():
-    """Takes arguments argv to list from database
-    Only lists with states that matches name argument
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
+def getStates(userName, passWord, dbName, stateName):
+    """ Accesses database hbtn_0e_0_usa and grabs states that begin with 'N'.
+    ARGS:
+        userName: the username
+        passWord: the password
+        dbName: the name of the database to access
+        stateName: the name of the state to check
     """
-    if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
 
-        cur = db.cursor()
+    import MySQLdb
 
-        cur.execute("SELECT cities.name FROM cities\
-                    JOIN states ON cities.state_id = states.id\
-                    AND states.name = '{:s}'\
-                    ORDER BY cities.id ASC".format(sys.argv[4]))
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=userName,
+        passwd=passWord,
+        db=dbName,
+        charset="utf8"
+    )
 
-        rows = cur.fetchall()
+    cur = db.cursor()
 
-        res = []
-        for i in rows:
-            res.append(i[0])
-
-        print(", ".join(res))
-
-        cur.close()
-        db.close()
+    cur.execute("SELECT cities.name FROM cities "
+                "JOIN states ON states.id = cities.state_id "
+                "WHERE states.name = %s;", (stateName,))
+    query_rows = cur.fetchall()
+    print(", ".join([i[0] for i in query_rows]))
+    cur.close()
+    db.close()
 
 if __name__ == "__main__":
-    list_cities()
+    """ Take in arguments and passes to get states from db """
+    from sys import argv
+
+    getStates(argv[1], argv[2], argv[3], argv[4])
